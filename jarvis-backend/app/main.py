1a5b575139c27ecc141af44fe194ea0176ed8b25
Manash Anand <anandmanash321@gmail.com>
2026-05-03 04:11:55 +0530

    feat: added sqlite/memory.db with message table

--- jarvis-backend/app/main.py
+++ jarvis-backend/app/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import router
 from dotenv import load_dotenv
-from .db.db_config import init_db
+from .db_config import init_db
 from contextlib import asynccontextmanager
 from .services.helper import clear_audio_files
 
 load_dotenv()
 
-    
+
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     print("--------------------------")
